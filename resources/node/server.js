90450bdbd4d90ff53233e799c4dd24b9ba4c38f5
JP Gorman <j.p.g.gorman@mgail.com>
2016-12-28 18:05:02 +0000

    Remove default cookies

--- resources/node/server.js
+++ resources/node/server.js
@@ -3,6 +3,9 @@ var path = require("path")
 var open = require("open")
 var compression = require("compression")
 var helmet = require("helmet")
+var session = require("cookie-session")
+
+var expiryDate = new Date(Date.now() + 60 * 60 * 1000) // 1 hour
 // import favicon from "serve-favicon"
 
 /*eslint-disable no-console */
@@ -11,6 +14,17 @@ const port = process.env.PORT || 8887
 const host = process.env.HOST || "http://localhost"
 const app = express()
 
+app.use(session({
+  name: "session",
+  keys: ["key1"],
+  cookie: {
+    secure: true,
+    httpOnly: true,
+    domain: "catchappell.co.uk",
+    path: "/",
+    expires: expiryDate
+  }
+}))
 app.use(helmet())
 app.use(compression())
 app.use(express.static("public"))

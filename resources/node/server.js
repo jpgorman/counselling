84fcc1b1a56b8355f9e32d34b92dec03edc30949
JP Gorman <j.p.g.gorman@mgail.com>
2016-12-28 17:54:31 +0000

    Add helmet to express server

--- resources/node/server.js
+++ resources/node/server.js
@@ -2,6 +2,7 @@ var express = require("express")
 var path = require("path")
 var open = require("open")
 var compression = require("compression")
+var helmet = require("helmet")
 // import favicon from "serve-favicon"
 
 /*eslint-disable no-console */
@@ -10,12 +11,18 @@ const port = process.env.PORT || 8887
 const host = process.env.HOST || "http://localhost"
 const app = express()
 
+app.use(helmet())
 app.use(compression())
 app.use(express.static("public"))
 // app.use(favicon(path.join(__dirname,"assets","public","favicon.ico")))
 
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "../../public/index.html"))
+  try {
+    res.sendFile(path.join(__dirname, "../../public/index.html"))
+  } catch (e) {
+    res.status(500).send("Internal Server Error")
+  }
+
 })
 
 app.listen(port, function(err) {

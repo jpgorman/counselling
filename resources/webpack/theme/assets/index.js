3d7c3a5e1ee5ac658d82f66ee99329bc4f019e16
JP Gorman <j.p.g.gorman@mgail.com>
2017-03-08 08:15:14 +0000

    Add first blog post

--- resources/webpack/theme/assets/index.js
+++ resources/webpack/theme/assets/index.js
@@ -1,6 +1,7 @@
 // note that this assets are copied into the build using copy-webpack-plugin
 module.exports = {
   logo: "assets/logo.jpg",
+  blog1: "assets/blog1.jpg",
   headShot: "assets/head-shot.jpg",
   outdoorShot: "assets/outdoor-shot.jpg",
   roomShot: "assets/therapy-room.jpg",
